bb3b8bb4d142037fb1e43707c09304bda6e413e9
Perttu Vierimaa <perttu.vierimaa@gmail.com>
2026-09-15 01:18:01 +0300

    UI

--- src/code/config.py
+++ src/code/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     ollama_host: str = "https://ollama.pepevapp.com" #server
-    default_model: str = "qwen2.5-coder:3b"
+    default_model: str = "qwen2.5:1.5b"
     max_tokens: int = 2048
     SYSTEM_PROMPT: str = "You are computer supply chain risk and escalation alarm. You output the necessary details and info based on what you will receive from the user."
 
