bec603e3cff72a84634b48e8bf54308ae85f4391
Perttu Vierimaa <perttu.vierimaa@gmail.com>
2026-09-15 13:01:00 +0300

    Improve config to parse .env

--- src/ai/llm.py
+++ src/ai/llm.py
@@ -1,15 +1,16 @@
 from httpx import AsyncClient
-from src.code.config import settings
+from src.code.config import Settings
 from src.code.logger import logger
 
 async def receive_messages(messages):
+    settings = Settings()
     try:
         full_messages = [{"role": "system", "content": settings.SYSTEM_PROMPT}] + messages
         async with AsyncClient(timeout=240) as client:
             response = await client.post(
                 f"{settings.ollama_host}/api/chat",
                 json={
-                    "model": settings.default_model,
+                    "model": settings.model,
                     "messages": full_messages,
                     "stream": False,
                 }
--- src/code/config.py
+++ src/code/config.py
@@ -1,4 +1,5 @@
 import dotenv
+from src.code.logger import logger
 
 class Settings():
     env_file = ".env"
@@ -6,8 +7,7 @@ class Settings():
 
     def __init__(self):
         loaded_conf = dotenv.dotenv_values(self.env_file)
-        self.ollama_host = loaded_conf.get('ollamaHost', "https://ollama.pepevapp.com" )
-        self.model = loaded_conf.get('defaultModel', "qwen2.5:1.5b")
-        print(f'LOADED MODEL: {self.model}')
-        self.max_tokens = loaded_conf.get('maxTokens', 2048)
-        self.system_prompt = loaded_conf.get('systemPrompt', self.SYSTEM_PROMPT)
+        self.ollama_host = loaded_conf.get('OLLAMA_HOST', "https://ollama.pepevapp.com" )
+        self.model = loaded_conf.get('MODEL', "qwen2.5:1.5b")
+        self.max_tokens = loaded_conf.get('MAX_TOKENS', 2048)
+        self.system_prompt = loaded_conf.get('SYSTEM_PROMPT', self.SYSTEM_PROMPT)
