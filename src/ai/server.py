8b268bbfbd7394e67951d2edef8d912baaf38e33
Perttu Vierimaa <perttu.vierimaa@gmail.com>
2026-09-15 12:42:15 +0300

    updated cofig

--- src/ai/server.py
+++ src/ai/server.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, Form, HTTPException
+from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse, PlainTextResponse
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
 
 from src.ai.narrator import narrator
 from src.ai.parser import parse_event
 from src.code.score import analyze_delay_event
-from src.code.config import settings
 from src.code.logger import logger
 from src.code.delay import get_dep, nodes
 
@@ -14,21 +12,6 @@ from src.ai.llm import receive_messages
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
-
-class ChatRequest(BaseModel):
-    messages: list
-    model: str = settings.default_model
-
-#Basic fastapi post for ollama chat endpoint, with error handling for failed requests.
-@app.post("/chat")
-async def chat(request: ChatRequest):
-    try:
-        #response = await 
-        response = await receive_messages(request.messages)
-        return response
-    except Exception as e:
-            logger.error(f"Error during chat request: {e}")
-            raise HTTPException(status_code=502, detail=str(e))
 
 @app.get("/")
 async def home():
--- src/code/config.py
+++ src/code/config.py
@@ -1,12 +1,13 @@
-from pydantic_settings import BaseSettings
+import dotenv
 
-class Settings(BaseSettings):
-    ollama_host: str = "https://ollama.pepevapp.com" #server
-    default_model: str = "qwen2.5:1.5b"
-    max_tokens: int = 2048
+class Settings():
+    env_file = ".env"
     SYSTEM_PROMPT: str = "You are computer supply chain risk and escalation alarm. You output the necessary details and info based on what you will receive from the user."
 
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
+    def __init__(self):
+        loaded_conf = dotenv.dotenv_values(self.env_file)
+        self.ollama_host = loaded_conf.get('ollamaHost', "https://ollama.pepevapp.com" )
+        self.model = loaded_conf.get('defaultModel', "qwen2.5:1.5b")
+        print(f'LOADED MODEL: {self.model}')
+        self.max_tokens = loaded_conf.get('maxTokens', 2048)
+        self.system_prompt = loaded_conf.get('systemPrompt', self.SYSTEM_PROMPT)
